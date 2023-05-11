fb66aaa4045ec80bb2f418d8026e782ac3829d81
nikola.svetic <af7er.g@gmail.com>
2023-05-11 21:41:30 +0200

    update

--- my/src/App.js
+++ my/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Messages, Input } from './Components';
 import { useState, useEffect } from 'react';
 
-
+/* 
 function membersName() {
   const adjectives = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
   const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
@@ -11,7 +11,7 @@ function membersName() {
   const noun = nouns[Math.floor(Math.random() * nouns.length)];
   return adjective + noun;
 
-}
+} */
 
   function randomName() {
     const primary = 'User';
@@ -90,7 +90,7 @@ export default function App() {
     <div className='App'>
 
       <div className='App-header'>
-        <h1 className='Ni'>My Chat App</h1>
+        <h1>My Chat App</h1>
       </div>
 
       <Messages messages={messages} currentMember={member} />
